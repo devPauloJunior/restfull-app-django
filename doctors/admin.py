7f3653c0987459355e350e7ddcbc49055e39bfd7
Paulo Junior <npaulo.ce@gmail.com>
2023-06-22 17:38:49 -0300

    update API

--- doctors/admin.py
+++ doctors/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Doctors
 
-# class ListDoctors(admin.ModelAdmin):
-#     list_display = ('doctors_id','doctors_last_name','doctors_first_name','doctors_type_blood')
+class ListDoctors(admin.ModelAdmin):
+    list_display = ('doctors_id','doctors_last_name','doctors_first_name','doctors_type_blood')
 
-# admin.site.register(Doctors, ListDoctors)
+admin.site.register(Doctors, ListDoctors)
